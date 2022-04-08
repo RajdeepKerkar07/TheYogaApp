f478007bfcc184f1f41d32495021e50f02da7b4d
evilemperor07 <rajdeepkerkar.com>
2022-04-08 17:31:00 +0530

    Renamed to asanas

--- TheYogaApp/TheYogaApp/urls.py
+++ TheYogaApp/TheYogaApp/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('ExerciseAssistant.urls')),
+    path('', include('asanasC.urls')),
     path('admin/', admin.site.urls),
 ]

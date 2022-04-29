623d3de3b0d6eb5430bb7a87d674860e93bd0582
poonampai <poopai2000@gmail.com>
2022-04-29 21:20:18 +0530

    dashboard

--- TheYogaApp/asanas/views.py
+++ TheYogaApp/asanas/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from .models import Asana
+
 
 def index(request):
     return render(request, 'index.html')
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    asana = Asana.objects.all()
+    return render(request, 'dashboard.html', {'asana': asana})
+

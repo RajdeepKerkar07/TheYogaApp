3f7c30e46a38748fe045dc454fe96837e54b0fd5
evilemperor07 <rajdeepkerkar.com>
2022-06-28 16:11:36 +0530

    Yoga Integration

--- TheYogaApp/asanas/views.py
+++ TheYogaApp/asanas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
 from .models import Asana
+import os
+from django.conf import settings
 
 
 def index(request):
@@ -11,3 +12,9 @@ def dashboard(request):
     asana = Asana.objects.all()
     return render(request, 'dashboard.html', {'asana': asana})
 
+
+def yoga(request):
+    return render(request, 'yoga.html')
+
+
+# file = open(os.path.join(settings.PROJECT_ROOT, 'images/mountain.svg'))
